d20057cf18cb1576f013eaae4bc53f301df9358f
Fahtheemah <bukky.kabiru.bk@gmail.com>
2023-09-01 12:18:26 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
+/**
+ * check_num - Check if a string contains only digits
+ * @str: The sting to check
+ *
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
 /**
  * main - adds positive numbers
  * @argc: number of arguments
- * @argv: Array containing the arguments
+ * @argv: Array containing arguments
+ *
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	count = 1;
+	while (count < argc)
 	{
-		for (j = 0; argv[i][j] != '\0';)
+		if (check_num(argv[count]))
+
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
 		}
-		add += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
 	}
-	printf("%d\n", add);
+
+	printf("%d\n", sum);
+
 	return (0);
 }
