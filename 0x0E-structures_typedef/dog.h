ea4ef0b182128a04f85d46d1d8d803d260a0a69f
Fahtheemah <bukky.kabiru.bk@gmail.com>
2023-09-11 07:23:11 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - basic info of dogs
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Description: to group basic info of the dogs
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
